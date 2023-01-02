5c703be5f61873e6fdd70b6aed8d79d2e374ef1e
soumya362 <121574902+soumya362@users.noreply.github.com>
2023-01-02 16:39:49 +0530

    Update Student.py

--- Student.py
+++ Student.py
@@ -1,111 +1,86 @@
-import csv 
-     
- def student_add(): 
-     with open("student.csv", "a") as obj: 
-         wobj=csv.writer(obj) 
-         while True: 
-             id=input("Enter Student ID ") 
-             name=input("Enter Name ") 
-             roll=input("Enter Class Roll Number ") 
-             batch=input("Enter Batch Name ") 
-             record=[id,name,roll,batch] 
-             wobj.writerow(record) 
-             ch=input("exit to exit, any other key to continue ") 
-             if ch=="exit": 
-                 break 
-  
- def student_display(): 
-     with open("student.csv", "r") as obj: 
-         obj.seek(0) 
-         robj=csv.reader(obj) 
-         for i in robj: 
-             print(i) 
-  
- def student_update(): 
-     with open("student.csv", "r") as obj: 
-         robj=csv.reader(obj)     
-         L=[] 
-         found=False 
-         fid = input("Enter Student ID to edit data ") 
-         for i in robj: 
-             if i[0]==fid: 
-                 found=True 
-                 nid=input("Enter new Student ID ") 
-                 name=input("Enter new Name ") 
-                 roll=input("Enter new Class Roll Number ") 
-                 batch=input("Enter new Batch Name ") 
-                 i[0]=nid 
-                 i[1]=name 
-                 i[2]=roll 
-                 i[3]=batch 
-             else: 
-                 print("Student not found") 
-             L.append(i) 
-  
-     if found==False: 
-         print("Student cannot be found") 
-     else: 
-         with open("student.csv", "w+", newline="") as obj: 
-             wobj=csv.writer(obj) 
-             wobj.writerows(L) 
-             obj.seek(0) 
-             robj=csv.reader(obj) 
-             for i in robj: 
-                 print(i) 
-  
- def student_delete(): 
-     with open("student.csv", "r") as obj: 
-         robj=csv.reader(obj) 
-         L=[] 
-         found=False 
-         fid = input("Enter Student ID to delete data ") 
-         for i in robj: 
-             if i[0]==fid: 
-                 found=True 
-             else: 
-                 print("Student not found") 
-                 L.append(i) 
-  
-     if found==False: 
-         print("Student cannot be found") 
-     else: 
-         with open("student.csv", "w+", newline="") as obj: 
-             wobj = csv.writer(obj) 
-             wobj.writerows(L) 
-             obj.seek(0) 
-             robj = csv.reader(obj) 
-             for i in robj: 
-                 print(i) 
-  
- def report_card(): 
-     name=input("Enter name of student ") 
-     a="report_card_"+name 
-     with open(a+".txt","w") as obj: 
-         num=int(input("enter number of subjects ")) 
-         for i in range(num): 
-             sub=input("enter subject ") 
-             marks=int(input("enter marks out of 100 ")) 
-             if marks>=90: 
-                 grade="A" 
-             elif marks>=80: 
-                 grade="B" 
-             elif marks>=70: 
-                 grade="C" 
-             elif marks>=60: 
-                 grade="D" 
-             elif marks>=50: 
-                 grade="E" 
-             else: 
-                 grade="F" 
-             if grade == "F": 
-                 result = "FAIL" 
-             else: 
-                 result = "PASS" 
-             k=[sub," ",str(marks)," ",str(grade)," ",result,"\n"] 
-             obj.writelines(k) 
-  
- #student_add() 
- #student_display() 
- #student_update() 
- #student_delete() 
- #report_card()
+
+import mysql.connector
+	import json
+	mydb = mysql.connector.connect(host = "localhost", user = "root", password = "The39clues$", database = "student")
+	cursorObject = mydb.cursor()
+	
+	def createStudent(student_id, name):
+	class_roll_number = int(student_id[5:7])
+	batch_id = student_id[:5]
+	sql = "INSERT INTO student (student_id, name, class_roll_number, batch_id) VALUES (%s, %s, %s, %s)"
+	val = (student_id, name, class_roll_number, batch_id)
+	cursorObject.execute(sql, val)
+	mydb.commit()
+	
+	def updateStudent(ostudent_id, nstudent_id, name):
+	class_roll_number = int(nstudent_id[5:7])
+	batch_id = nstudent_id[0:5]
+	sql = "UPDATE student SET student_id = %s, name = %s, class_roll_number = %s, batch_id = %s WHERE student_id = %s"
+	val = (nstudent_id, name, class_roll_number, batch_id, ostudent_id)
+	cursorObject.execute(sql, val)
+	mydb.commit()
+	
+	def removeStudent(student_id):
+	sql = "DELETE FROM student WHERE student_id = %s"
+	val = (student_id, )
+	cursorObject.execute(sql, val)
+	mydb.commit()
+	
+	def reportCard(student_idx):
+	cursorObject.execute("SELECT * FROM student")
+	result = cursorObject.fetchall()
+	students = []
+	student_ids = []
+	for i in range(0, len(result)):
+	student_ids.append(result[i][0])
+	students.append(result[i][1])
+	check = 0
+	for i in range(0, len(student_ids)):
+	if(student_idx == student_ids[i]):
+	check = 1
+	break
+	if(check == 1):
+	studentx = students[i]
+	mydb1 = mysql.connector.connect(host = "localhost", user = "root", password = "The39clues$", database = "course")
+	cursorObject1 = mydb1.cursor()
+	cursorObject1.execute("SELECT * FROM course")
+	result = cursorObject1.fetchall()
+	marks = []
+	subjects = []
+	for i in range(0, len(result)):
+	marks.append(json.loads(result[i][2]))
+	subjects.append(result[i][1])
+	file_name = student_idx + ".txt"
+	file = open(file_name, "w+")
+	student_id = "Student ID: " + student_idx + " \n"
+	name = "Name: " + studentx + " \n"
+	L = [student_id, name]
+	file.writelines(L)
+	total_marks = 0
+	divs = 0
+	for i in range(0, len(subjects)):
+	temp = marks[i]
+	if(isinstance(temp.get(student_idx), int)):
+	subject_marks = "Marks in " + subjects[i] + ": " + str(temp.get(student_idx)) + "% \n"
+	divs += 1
+	total_marks += temp.get(student_idx)
+	file.write(subject_marks)
+	grade = "Grade obtained: " + gradeCheck(total_marks/divs) + " \n"
+	file.write(grade)
+	file.close()
+	else:
+	print("Student ID does not exist")
+	
+	def gradeCheck(a):
+	if(a >= 90):
+	return "A"
+	elif(a >= 80):
+	return "B"
+	elif(a >= 70):
+	return "C"
+	elif(a >= 60):
+	return "D"
+	elif(a >= 50):
+	return "E"
+	else:
+	return "F"
