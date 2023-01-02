9d401e70268a32723d83e79de604f2c815ed4ce5
soumya362 <121574902+soumya362@users.noreply.github.com>
2023-01-02 16:35:42 +0530

    Update Course.py

--- Course.py
+++ Course.py
@@ -1,50 +1,95 @@
-import csv 
- import matplotlib.pyplot as plt 
-  
- def course_add(): 
-     with open("course.csv", "a",newline="") as obj: 
-         wobj=csv.writer(obj) 
-         while True: 
-             id=input("Enter Course ID ") 
-             name=input("Enter Course Name ") 
-             data=[id,name] 
-             wobj.writerow(data) 
-             while True: 
-                 sname=input("Enter Student Name ") 
-                 sroll=int(input("Enter Roll Number ")) 
-                 smarks=int(input("Enter Marks ")) 
-                 record=[sname,str(sroll),str(smarks)] 
-                 wobj.writerow(record) 
-                 ch = input("exit to exit, any other key to continue ") 
-                 if ch == "exit": 
-                     break 
-             ch=input("exit to exit, any other key to continue ") 
-             if ch=="exit": 
-                 break 
-  
- def display_courses(): 
-     with open("course.csv", "r") as obj: 
-         obj.seek(0) 
-         robj=csv.reader(obj) 
-         for i in robj: 
-             print(i) 
-  
- def histogram(): 
-     l=[] 
-     with open("course.csv","r") as obj: 
-         robj=csv.reader(obj) 
-         for i in robj: 
-             if len(i)==3: 
-                 l.append(int(i[2])) 
-         print(l) 
-     k=[0,10,20,30,40,50,60,70,80,90,100] 
-     plt.hist(l,bins=k,rwidth=0.8) 
-     plt.xlabel("GRADES") 
-     plt.ylabel("NUMBER OF STUDENTS") 
-     plt.title("COURSE STATISTICS") 
-     plt.xticks(k) 
-     plt.show() 
-  
- #course_add() 
- #display_courses() 
- #histogram()
+
+
+import mysql.connector
+	import json
+	import pandas
+	import matplotlib.pyplot
+	from collections import Counter
+	from student import gradeCheck
+	mydb = mysql.connector.connect(host = "localhost", user = "root", password = "The39clues$", database = "course")
+	cursorObject = mydb.cursor()
+	
+	def createCourse(course_id, course_name):
+	print("Enter students in course: ")
+	marks_obtained_D = {}
+	while(True):
+	student_id = input("Enter student ID (to stop enter STOP): ")
+	if(student_id.upper() == "STOP"):
+	break
+	else:
+	marks = int(input("Enter marks obtained in course: "))
+	marks_obtained_D[student_id] = marks
+	marks_obtained = json.dumps(marks_obtained_D)
+	sql = "INSERT INTO course (course_id, course_name, marks_obtained) VALUES (%s, %s, %s)"
+	val = (course_id, course_name, marks_obtained)
+	cursorObject.execute(sql, val)
+	mydb.commit()
+	
+	def checkPerformance(course_id):
+	cursorObject.execute("SELECT * FROM course")
+	myresult = cursorObject.fetchall()
+	check = 0
+	data = []
+	for i in range(0, len(myresult)):
+	if(myresult[i][0] == course_id):
+	check = 1
+	student_marks_d = json.loads(myresult[i][2])
+	student_marks = [(k, v) for k, v in student_marks_d.items()]
+	mydb1 = mysql.connector.connect(host = "localhost", user = "root", password = "The39clues$", database = "student")
+	cursorObject1 = mydb1.cursor()
+	cursorObject1.execute("SELECT * FROM student")
+	result = cursorObject1.fetchall()
+	student_ids = []
+	students = []
+	student_rolls = []
+	for j in range(0, len(result)):
+	student_ids.append(result[j][0])
+	students.append(result[j][1])
+	student_rolls.append(result[j][2])
+	student_datas = [student_ids, students, student_rolls]
+	for j in range(0, len(student_marks)):
+	for k in range(0, len(student_datas[0])):
+	if(student_marks[j][0] == student_datas[0][k]):
+	print("Student ID: " + student_datas[0][k])
+	print("Student Name: " + student_datas[1][k])
+	print("Student Roll Number: " + str(student_datas[2][k]))
+	print("Marks obtained: " + str(student_marks[j][1]))
+	print()
+	data.append([student_datas[0][k],student_datas[1][k],student_datas[1][k],student_datas[2][k],student_marks[j][1]])
+	if(check == 0):
+	print("Course does not exist")
+	return data
+	
+	def courseStatistics(course_id):
+	mydb1 = mysql.connector.connect(host = "localhost", user = "root", password = "The39clues$", database = "student")
+	cursorObject1 = mydb1.cursor()
+	cursorObject1.execute("SELECT * FROM student")
+	result = cursorObject1.fetchall()
+	student_ids = []
+	for i in range(0, len(result)):
+	student_ids.append(result[i][0])
+	cursorObject.execute("SELECT * FROM course")
+	result = cursorObject.fetchall()
+	marks = []
+	course_ids = []
+	for i in range(0, len(result)):
+	marks.append(json.loads(result[i][2]))
+	course_ids.append(result[i][0])
+	grades = []
+	for i in range(0, len(student_ids)):
+	x = None
+	for j in range(0, len(course_ids)):
+	temp = marks[j]
+	if(isinstance(temp.get(student_ids[i]), int)):
+	if(course_ids[j] == course_id):
+	x = temp.get(student_ids[i])
+	if(isinstance(x, int)):
+	grades.append(gradeCheck(x))
+	if(len(grades) == 0):
+	print("Course does not exist")
+	else:
+	grades.sort()
+	letter_counts = Counter(grades)
+	df = pandas.DataFrame.from_dict(letter_counts, orient='index')
+	df.plot(kind='bar')
+	matplotlib.pyplot.show()
